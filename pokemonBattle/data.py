ef45361fcb31ad0943c5b7952698d296f50569a6
mike <luismfialho@outlook.com>
2024-10-17 18:26:31 +0100

    commiting some small adjustements to codebase

--- pokemonBattle/data.py
+++ pokemonBattle/data.py
@@ -4,11 +4,11 @@ import pandas as pd
 
 data = pd.read_csv('/home/mike/Coding_Projects/JS_Projects/Vite/PokemonDev/PokemonAI/archive(1)/Pokedex_Ver_SV2.csv')
 
-columnsToDrop = ['Original_Name','Branch_Code','Generation','Height','Weight','Color','Gender_Male','Gender_Female','Gender_Unknown','Egg_Steps','Egg_Group1','Egg_Group2','Get_Rate','Base_Experience','Experience_Type','Category','Mega_Evolution_Flag','Region_Form','E_HP','E_Attack','E_Defense','E_SP_Attack','E_SP_Defense','E_Speed','Ability1','Ability2','Ability_Hidden','HP','Attack','Defense','SP_Attack','SP_Defense','Speed','Total']
+columnsToDrop = ['Original_Name','Branch_Code','Generation','Height','Weight','Color','Gender_Male','Gender_Female','Gender_Unknown','Egg_Steps','Egg_Group1','Egg_Group2','Get_Rate','Base_Experience','Experience_Type','Category','Mega_Evolution_Flag','Region_Form','E_HP','E_Attack','E_Defense','E_SP_Attack','E_SP_Defense','E_Speed','Ability1','Ability2','Ability_Hidden']
 
 
 
 data.drop(columnsToDrop, inplace=True, axis=1)
 
-PDataset = 'pDatasetSimple.csv'
+PDataset = 'PokemonDSETStats.csv'
 data.to_csv(PDataset, index=False)
